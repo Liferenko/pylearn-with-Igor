386e6e210c5c341d49a0e35b6c6d7be4ef88bff7
Pavel Liferenko <eleven.krsk@gmail.com>
2016-11-12 10:12:50 +0200

    первый коммит до чистки компа

---
+++ facebook/api.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+import requests
+
+"""
+найти функции и запросы, которые нужны нам для использования от Фейсбука. Какие запросы фб должен получить для выполнения операции.
+прочитать что такое requests - http://docs.python-requests.org/en/master/
+
+подготовить авторизацию для приложения
+"""
+
+def request_by_url(url, method='get'):
+    pass
