cbc70e2c6d0753e1c0f5b1c76fdd5d901c119e36
Pavel Liferenko <eleven.krsk@gmail.com>
2016-11-19 17:43:47 +0200

    Наконец-то смог вытянуть токен из JSONа. Плюс добавил бутстреп для шаблона страницы Была проблема - съехали табы у user_code и это выдавало ошибку. То есть if user_code выполнялся только с else

--- app/views.py
+++ app/views.py
@@ -16,16 +16,18 @@ def index():
               }
     if user_code:
         result = requests.get('https://graph.facebook.com/v2.8/oauth/access_token',
-                     params={'client_id': params['client_id'],
-                             'redirect_uri': params['redirect_uri'],
-                             'client_secret': '756ff1cf3e2bf9a7e6c017fc8202ed5c',
-                             'code': user_code}) # тут я волен доставлять параметры и заданные мной значения. Как я понял, к примеру могу поставить "паузаРекламы=тру"
+                              params={'client_id': params['client_id'],
+                                      'redirect_uri': params['redirect_uri'],
+                                      'client_secret': '756ff1cf3e2bf9a7e6c017fc8202ed5c',
+                                      'code': user_code})
         raw_FBAccessToken = result.content.decode()
         raw_FBAccessToken_to_JSON = json.loads(raw_FBAccessToken)
         access_token = raw_FBAccessToken_to_JSON['access_token']
-        print(access_token)
+        print('\n---\nЯ успешно получил акцес_токен\n')
     else:
         print('Что-то пошло не так в user_code')
+
+
 
 
 
